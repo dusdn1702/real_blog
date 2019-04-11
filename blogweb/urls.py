3ca1dbe262e278691313b392ea3eb5dc539682f9
dusdn1702 <dusdn1702@naver.com>
2019-04-11 18:58:55 +0900

    asdf

---
+++ blogweb/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path, include
+import blogweb.views
+
+
+urlpatterns =[
+    path('home', blogweb.views.home, nerrame='home'),
+    path('', blogweb.views.post, name='post'),
+    path('post/<int:post_id>/', blogweb.views.detail, name='detail'),
+    path('post/new/', blogweb.views.new, name='new'),
+    path('post/create/', blogweb.views.create, name='create'),
+]
